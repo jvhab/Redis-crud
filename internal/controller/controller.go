922ffeac92fa7090c0865e79063e298fe22462ba
Basyrov Konstantin <jvhab@yandex.ru>
2023-04-16 22:51:49 +0500

    Init controller

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -1,6 +1,11 @@
 package controller
 
-import "github.com/jvhab/Redis-crud/internal/repository"
+import (
+	"context"
+	"github.com/google/uuid"
+	"github.com/jvhab/Redis-crud/internal/model"
+	"github.com/jvhab/Redis-crud/internal/repository"
+)
 
 type Controller struct {
 	rep repository.RedisRepository
@@ -10,4 +15,20 @@ func NewController(ctrl repository.RedisRepository) *Controller {
 	return &Controller{
 		rep: ctrl,
 	}
+}
+
+func (c *Controller) Create(ctx context.Context, news *model.News) error {
+	return c.rep.Create(ctx, news)
+}
+
+func (c *Controller) Update(ctx context.Context, news *model.News) error {
+	return c.rep.Update(ctx, news)
+}
+
+func (c *Controller) Get(ctx context.Context, id uuid.UUID) (*model.News, error) {
+	return c.rep.Get(ctx, id)
+}
+
+func (c *Controller) Delete(ctx context.Context, id uuid.UUID) error {
+	return c.rep.Delete(ctx, id)
 }
