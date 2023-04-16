84ad14297cca1d8a6662e35bd6cea334094a9f28
Basyrov Konstantin <jvhab@yandex.ru>
2023-04-16 22:05:38 +0500

    add repo

--- internal/repository/redis_repository.go
+++ internal/repository/redis_repository.go
@@ -20,21 +20,21 @@ func NewRepository(redisClient *redis.Client) *Repository {
 	}
 }
 
-func (r *Repository) Create(ctx context.Context, news *model.News) error {
+func (r *Repository) Create(ctx context.Context, news *model.News) (uuid.UUID, error) {
 	id, err := uuid.NewUUID()
 	if err != nil {
-		return errors.Wrap(err, "Repository.Create.uuid.NewUUID")
+		return uuid.Nil, errors.Wrap(err, "Repository.Create.uuid.NewUUID")
 	}
 	news.Id = id
 	newsBytes, err := json.Marshal(news)
 	if err != nil {
-		return errors.Wrap(err, "Repository.Create.json.Marshal")
+		return uuid.Nil, errors.Wrap(err, "Repository.Create.json.Marshal")
 	}
 	if err := r.redisClient.Set(ctx, id.String(), newsBytes, time.Second*3).Err(); err != nil {
-		return errors.Wrap(err, "Repository.Create.redis.Set")
+		return uuid.Nil, errors.Wrap(err, "Repository.Create.redis.Set")
 	}
 
-	return nil
+	return id, nil
 }
 
 func (r *Repository) Update(ctx context.Context, news *model.News) error {
@@ -50,9 +50,22 @@ func (r *Repository) Update(ctx context.Context, news *model.News) error {
 }
 
 func (r *Repository) Get(ctx context.Context, id uuid.UUID) (*model.News, error) {
-	return nil, nil
+	result := &model.News{}
+	res, err := r.redisClient.Get(ctx, id.String()).Bytes()
+	if err != nil {
+		return &model.News{}, errors.Wrap(err, "Repository.Get.redis.Get")
+	}
+	if err := json.Unmarshal(res, result); err != nil {
+		return &model.News{}, errors.Wrap(err, "Repository.Get.json.Unmarshal")
+	}
+
+	return result, nil
 }
 
 func (r *Repository) Delete(ctx context.Context, id uuid.UUID) error {
+	if err := r.redisClient.Del(ctx, id.String()).Err(); err != nil {
+		return errors.Wrap(err, "Repository.Del.redis.Del")
+	}
+
 	return nil
 }
